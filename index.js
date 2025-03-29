268599a48b57ca73603e62f78b6e8abe22c8ad56
jerome <jeromeabelida19@gmail.com>
2025-03-29 23:40:12 +0800

    added: sign_in and sign_up routes

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/sign_in", async (req, res) => {
+  console.log(req.body.username);
+});
+
+app.post("/sign_up", async (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.password);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on localhost: ${port}`);
-})
+});
